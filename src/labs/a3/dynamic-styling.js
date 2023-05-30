685f2468dc41f5b90a1cfb04aa95af429d58cff0
Srinidhi Sunkara <srinidhisunkara@Srinidhis-MBP.attlocal.net>
2023-05-30 11:58:39 -0500

    JavaScript and React.js Assignment 3

---
+++ src/labs/a3/dynamic-styling.js
@@ -0,0 +1,5 @@
+import React from "react";
+function DynamicStyling(){
+
+}
+export default DynamicStyling;
---
+++ src/tuiter/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import Nav from "../nav";
+import { Routes, Route } from "react-router";
+import NavigationSidebar from "./navigation-sidebar";
+import HomeScreen from "./home-screen";
+import BookmarksScreen from "./bookmarks-screen";
+import ProfileScreen from "./profile-screen";
+
+import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+import WhoToFollowList from "./who-to-follow-list";
+import ExploreScreen from "./explore-screen";
+
+
+
+function Tuiter() {
+    return(
+      //  <div>
+      //     <Nav/>
+      //     <h1>Tuiter</h1>
+      //  </div>
+      // <div>
+      //    <Nav />
+      //    <div className="row">
+      //       <div className="col-2">
+      //          <NavigationSidebar />
+      //       </div>
+      //       <div className="col-7">
+      //          <Routes>
+      //          <Route path="/home" element={<HomeScreen/>} />
+      //          <Route path="/explore" element={<ExploreScreen/>} />
+      //          <Route path="/bookmarks" element={<BookmarksScreen/>}/>
+      //          <Route path="/profile" element={<ProfileScreen/>}/>
+      //          </Routes>
+      //       </div>
+      //       <div className="col-3">
+      //          <h1>Who to follow</h1>
+      //       </div>
+      //    </div>
+      // </div>
+
+      <div className="row">
+         <div className="col-2">
+            <NavigationSidebar />
+         </div>
+         <div className="col-7">
+            <Routes>
+            <Route path="/home" element={<HomeScreen />} />
+            <Route path="/explore" element={<ExploreScreen/>} />
+            <Route path="/bookmarks" element={<BookmarksScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+            </Routes>
+         </div>
+         <div className="col-3">
+         
+         <WhoToFollowList/>
+         </div>
+         </div>
+
+
+    );
+ }
+ export default Tuiter
